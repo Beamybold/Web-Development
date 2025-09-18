608ce53c6ca48bd32ccf9fc3422f317b78b31a2f
Adebimpe <atoyebiadebimpe01@gmail.com>
2025-09-18 11:38:06 +0100

    new task

--- HTML/Lessons and tasks/javascript/script.js
+++ HTML/Lessons and tasks/javascript/script.js
@@ -1,18 +1,27 @@
-const aboutme=prac.getElementById("aboutme")
-const mycontact=prac.getElementById("mycontact")
-const myskills=prac.getElementById("myskills")
-const about=prac.getElementById("about")
-const contact=prac.getElementById("contact")
-const skills=prac.getElementById("skills")
+const aboutme=document.getElementById("aboutme")
+const mycontact=document.getElementById("mycontact")
+const myskills=document.getElementById("myskills")
+const about=document.getElementById("about")
+const contact=document.getElementById("contact")
+const skills=document.getElementById("skills")
+
+
+
 
 aboutme.onclick= function(){
-    about.style.display = "block"
-    contact.style.display = "none"
-    skills.style.display = "none"
-}
-mycontact.onclick= function(){
-    about.style.display = "none"
-    contact.style.display = "block"
-    skills.style.display = "none"
-}
+    about.style.display="block";
+    contact.style.display="none";
+    skills.style.display="none";
+};
 
+mycontact.onclick= function(){
+    about.style.display="none";
+    contact.style.display="block";
+    skills.style.display="none";
+};
+
+myskills.onclick= function(){
+    about.style.display="none";
+    contact.style.display="none";
+    skills.style.display="block";
+};
