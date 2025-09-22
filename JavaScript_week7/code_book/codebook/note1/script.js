a533e94685c01ed1ceb5ce7a998eb894ee820842
Adebimpe <atoyebiadebimpe01@gmail.com>
2025-09-22 16:02:14 +0100

    new task

---
+++ JavaScript_week7/code_book/codebook/note1/script.js
@@ -0,0 +1,3 @@
+function runFirstProgram() {
+        document.getElementById('first-output').innerHTML= 'Hello World! This message was created by JavaScript';
+        console.log('Hello from JavaScript alert');
